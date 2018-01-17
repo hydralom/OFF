15525b20955e2dfc374c32792e59a4b3a6011650
U-FR\WP5269 <WP5269@SL10006628.fr.green.local>
2018-01-17 12:09:03 +0100

    ajout liste ingredient et additif dans modifier produit

--- src/pages/modifierProduit/modifierProduit.ts
+++ src/pages/modifierProduit/modifierProduit.ts
@@ -1,98 +1,43 @@
 import {Component} from '@angular/core';
-import {NavController, AlertController, NavParams} from 'ionic-angular';
-import { MenuController } from "ionic-angular";
+import {NavController, NavParams} from 'ionic-angular';
 
 @Component({
   selector: 'page-modifierProduit',
   templateUrl: 'modifierProduit.html'
 })
 export class ModifierProduitPage {
-  tabIngredients: Array<String> = [];
-  tabAdditifs: Array<String> = [];
-
-  activeMenu: string;
+  cardIngredient: Boolean = false;
+  cardAdditif: Boolean = false;
 
   selectedItem: any = [];
 
-  ingredients: String = "";
-  additifs: String = "";
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, public menu: MenuController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.selectedItem = navParams.get('item');
-    this.afficheMenuIngredient();
   }
 
-  addProd() {
-
+  ouvrirFermerCardIngredient(){
+    this.cardAdditif = false;
+    if( this.cardIngredient ) {
+      this.cardIngredient = false;
+    } else {
+      this.cardIngredient = true;
+    }
   }
 
-  addIngredients() {
-    let alert = this.alertCtrl.create({
-      title: 'Ingrédients',
-      message: 'Nom usuel de l\'ingrédient',
-      inputs: [
-        {
-          name: 'ingredient',
-          placeholder: 'Huile de ...'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Valider',
-          handler: data => {
-            this.tabIngredients.push(data.ingredient);
-            this.setIngredients();
-          }
-        },
-        {text: 'Annuler'}
-      ]
-
-    });
-    alert.present();
+  ouvrirFermerCardAdditif(){
+    this.cardIngredient = false;
+    if( this.cardAdditif ) {
+      this.cardAdditif = false;
+    } else {
+      this.cardAdditif = true;
+    }
   }
 
-  addAdditifs() {
-    let alert = this.alertCtrl.create({
-      title: 'Additifs',
-      message: 'Nom usuel de l\'additif',
-      inputs: [
-        {
-          name: 'additif',
-          placeholder: 'Acide lactique'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Valider',
-          handler: data => {
-            this.tabAdditifs.push(data.additif);
-            this.setAdditifs();
-          }
-        },
-        {text: 'Annuler'}
-      ]
-
-    });
-    alert.present();
+  boutonFermerCardIngredient(){
+    this.cardIngredient = false;
   }
 
-  setIngredients() {
-    //this.infoProd.ingredients = this.tabIngredients.toString();
-  }
-
-  setAdditifs() {
-    //this.infoProd.additives = this.tabAdditifs.toString();
-  }
-
-  afficheMenuIngredient(){
-    this.activeMenu = 'menuIngredient';
-    this.menu.enable(true, 'menuIngredient');
-    this.menu.enable(false, 'menuAdditif');
-  }
-
-  afficheMenuAdditif(){
-    this.activeMenu = 'menuAdditif';
-    this.menu.enable(true, 'menuAdditif');
-    this.menu.enable(false, 'menuIngredient');
+  boutonFermerCardAdditif(){
+    this.cardAdditif = false;
   }
 }
