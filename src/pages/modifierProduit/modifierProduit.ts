04c1c134844ebdcff2384aa3f94dd2a904fc462b
U-FR\WP5269 <WP5269@SL10006628.fr.green.local>
2018-01-17 11:00:33 +0100

    ajout du menu dans modifer ingredient + ajout borders dans la liste d'items page recherche

--- src/pages/modifierProduit/modifierProduit.ts
+++ src/pages/modifierProduit/modifierProduit.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, AlertController, NavParams} from 'ionic-angular';
+import { MenuController } from "ionic-angular";
 
 @Component({
   selector: 'page-modifierProduit',
@@ -9,13 +10,16 @@ export class ModifierProduitPage {
   tabIngredients: Array<String> = [];
   tabAdditifs: Array<String> = [];
 
+  activeMenu: string;
+
   selectedItem: any = [];
 
   ingredients: String = "";
   additifs: String = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, public menu: MenuController) {
     this.selectedItem = navParams.get('item');
+    this.afficheMenuIngredient();
   }
 
   addProd() {
@@ -78,5 +82,17 @@ export class ModifierProduitPage {
 
   setAdditifs() {
     //this.infoProd.additives = this.tabAdditifs.toString();
+  }
+
+  afficheMenuIngredient(){
+    this.activeMenu = 'menuIngredient';
+    this.menu.enable(true, 'menuIngredient');
+    this.menu.enable(false, 'menuAdditif');
+  }
+
+  afficheMenuAdditif(){
+    this.activeMenu = 'menuAdditif';
+    this.menu.enable(true, 'menuAdditif');
+    this.menu.enable(false, 'menuIngredient');
   }
 }
